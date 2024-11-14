ab7045a186b01879ae9fe7a637d81e55a632e477
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-11-14 12:57:23 +0800

    style: refactor CSS styles for ColorPicker component
    - Update CSS styles for the ColorPicker component
    - Add new styles for the color picker and swatches
    - Include styles for color modes and color codes
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -53,8 +53,94 @@ const activeCode = computed(() => {
 
 <style lang="scss" scoped>
 ul {
-  list-style-type: none; /* 移除項目符號 */
-  padding: 0; /* 移除內邊距 */
-  margin: 0; /* 移除外邊距 */
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.color-picker {
+  background-color: #fff;
+  border: 1px solid #dae4e9;
+  border-radius: 0.125rem;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  color: #596a73;
+  font-family:
+    BlinkMacSystemFont,
+    Helvetica Neue,
+    sans-serif;
+  padding: 1rem;
+}
+
+.swatches {
+  color: #fff;
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: -0.25rem -0.25rem 0.75rem;
+  padding: 0;
+}
+
+.swatch {
+  border-radius: 0.125rem;
+  cursor: pointer;
+  height: 2rem;
+  margin: 0.25rem;
+  position: relative;
+  width: 2rem;
+}
+
+.swatch::after {
+  border-radius: 0.125rem;
+  bottom: 0;
+  box-shadow: inset 0 0 0 1px #dae4e9;
+  content: '';
+  display: block;
+  left: 0;
+  mix-blend-mode: multiply;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+.swatch svg {
+  display: none;
+  color: #fff;
+  fill: currentColor;
+  margin: 0.5rem;
+}
+
+.swatch.active svg {
+  display: block;
+}
+
+.color-modes {
+  display: flex;
+  font-size: 1rem;
+  letter-spacing: 0.05rem;
+  margin: 0 -0.25rem 0.75rem;
+}
+
+.color-mode {
+  background: none;
+  border: none;
+  color: #9babb4;
+  cursor: pointer;
+  display: block;
+  font-weight: 700;
+  margin: 0 0.25rem;
+  padding: 0;
+  text-transform: uppercase;
+}
+
+.color-mode.active {
+  color: #364349;
+}
+
+.color-code {
+  border: 1px solid #dae4e9;
+  border-radius: 0.125rem;
+  color: #364349;
+  text-transform: uppercase;
+  padding: 0.75rem;
 }
 </style>
